584a3396ab682d70dd72d0ef19daf029e71b1503
wrg216 <wrg_216@163.com>
2022-06-20 11:05:45 +0800

    CPP some file

---
+++ CPP/CPP_Primer/add_num.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: Wrg
+ * @Date: 2022-06-20 10:59:17
+ * @LastEditors: Wrg
+ * @LastEditTime: 2022-06-20 11:01:41
+ */
+
+#include <iostream>
+
+int main()
+{
+    int a = 0, b = 0;
+    std::cout << "Please input two numbers: ";
+    std::cin >> a >> b;
+    std::cout << "The sum of " << a << " and " << b << " is " << a + b << std::endl;
+    return 0;
+}
---
+++ CPP/CPP_Primer/empty.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: Wrg
+ * @Date: 2022-06-20 10:20:22
+ * @LastEditors: Wrg
+ * @LastEditTime: 2022-06-20 10:39:24
+ */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
